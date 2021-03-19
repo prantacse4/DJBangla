56aef020f3a67c97d20f7a110c30f851fdd882cd
prantacse4 <pranta.cse4.bu@gmail.com>
2021-03-19 11:12:51 +0600

    Update 1.2.1

--- crudsearch/admin.py
+++ crudsearch/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Student, Project
 # Register your models here.
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'student_id', 'phone')
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'student', 'project_name', 'project_details', 'project_status')
+
